122e3706101f892e714bc24417555ed944d0456a
fiver <boston.mboston@gmail.com>
2017-12-26 19:14:26 -0500

    database creation

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,3 +19,10 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+from django.urls import include, path
+from django.contrib import admin
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
